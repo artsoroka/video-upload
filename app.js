d7616bb97b5db9ee9b2d50ad21fdb6ce6e8dfb0d
artsoroka <githubrepository@yandex.ru>
2015-07-07 16:00:13 +0000

    added file upload handler

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var bodyParser 	 = require('body-parser');
 var cookieParser = require('cookie-parser');  
 var app        	 = express(); 
 var config	     = require('./config'); 
+var ffmpeg      = require('ffmpeg'); 
 
 app.use(bodyParser.urlencoded({ extended: false })); 
 app.use(cookieParser()); 
@@ -14,15 +15,41 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
 app.get('/', function(req,res){
-    res.send('welocome to video file upload service '); 
-}); 
-
-app.get('/upload', function(req,res){
-    res.send('upload file form'); 
+    res.render('mainpage');  
 }); 
 
 app.post('/upload', function(req,res){
-    res.send('video file upload endpoint'); 
+    var formidable   = require('formidable'); 
+    var form = new formidable.IncomingForm();
+    form.encoding = 'utf-8';
+    form.uploadDir = './uploads'; 
+    form.keepExtensions = false;
+    
+    form.parse(req, function(err, fields, files) {
+      if( ! files.upload.size ) return res.status(400).send('file is empty'); 
+      
+      try {
+            new ffmpeg(files.upload.path, function (err, video) {
+                if (!err) {
+                    console.log('The video is ready to be processed');
+                    res.send({
+                        user: req.sessionID, 
+                        file: files.upload,
+                        meta: video.metadata
+                    }); 
+                } else {
+                    console.log('Error: ' + err);
+                }
+            });
+        } catch (e) {
+            console.log(e.code);
+            console.log(e.msg);
+            return res.send('error reading a file '); 
+        }
+      
+    });
+    
+    
 }); 
 
 app.get('/file/:name', function(req,res){
