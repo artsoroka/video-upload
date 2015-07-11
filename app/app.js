6bd93515d0ca0baf4a4c58d243ca722d2ad801c1
artsoroka <githubrepository@yandex.ru>
2015-07-11 19:59:09 +0000

    handling 404

--- app/app.js
+++ app/app.js
@@ -24,6 +24,13 @@ app.get('/file/:name', function(req,res){
     res.send('get file data: ' + req.params.name); 
 }); 
 
+app.use('*', function(req,res){
+    
+    res.status('404').send('no valid route found on the server'); 
+    
+}); 
+
+
 app.listen(config.port, function(){
     console.log('Application running on a port %d', config.port); 
 });
