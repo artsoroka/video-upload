7b0c75275236799ea00059d3717377f28adc301f
artsoroka <githubrepository@yandex.ru>
2015-07-12 19:56:49 +0000

    bootstrap template

--- app/public/main.js
+++ app/public/main.js
@@ -20,8 +20,9 @@ var clearInput = function(elementId){
 var renderSelectedFilesTable = function(files){
     
     var tableBody = document.getElementById('selectedFiles'); 
+    
     if( ! files || ! files.length ) return tableBody.innerHTML = '';   
-   
+    
     var rows = []; 
     var tbody = ''; 
     
@@ -42,29 +43,83 @@ var renderSelectedFilesTable = function(files){
     
 }; 
 
+var renderTable = function(data){
+    
+    var tableBody = document.getElementById('existingFiles'); 
+    
+    if( ! data || ! data.length ) return tableBody.innerHTML = '';   
+    
+    var rows = []; 
+    var tbody = ''; 
+    
+    for(var i = 0; i < data.length; i++ ){
+        var tr = '<tr>'; 
+        tr += '<td>' + data[i].file.name + '</td>'; 
+        tr += '<td>' + data[i].meta.video.container + '</td>';
+        tr += '<td>' + data[i].meta.duration.raw + '</td>';
+        tr += '<td>' + data[i].file.path + '</td>';
+        tr += '</tr>'; 
+        
+        rows.push(tr); 
+    }
+    
+    for(var r in rows){
+        tbody += rows[r];     
+    }
+    
+    tableBody.innerHTML = tbody;  
+    
+}; 
+
+$.fn.showMessage = function(msg){
+    this.text(msg); 
+    this.show(); 
+}; 
+
+var updateProgressBar = function(percent){
+    //document.getElementById('uploadStatus').innerText = percent; 
+    $('#uploadStatus').css('width', percent + '%' ); 
+}; 
+
 $('#filePicker').click(function(e){
+    e.preventDefault(); 
     $('#videoFile').trigger('click'); 
 }); 
 
 $('#clearSelectedFiles').click(function(e){
+    e.preventDefault(); 
     clearInput('#videoFile'); 
     renderSelectedFilesTable(null); 
+    $('#selectedFilesTable').hide(); 
+    $('#uploadMsg').hide(); 
+    
+}); 
+
+$('#uploadSelectedFiles').click(function(e){
+    e.preventDefault(); 
+    $('#uploadMsg').hide(); 
+    $('#fileUpload').submit(); 
 }); 
 
 $('#videoFile').change(function(e){
     var files = e.target.files; 
     
-    renderSelectedFilesTable(files);     
+    renderSelectedFilesTable(files);
+    $('#uploadMsg').hide(); 
+    $('#selectedFilesTable').show(); 
     
 }); 
 
 $('#fileUpload').submit(function(e){
     e.preventDefault(); 
     
-    var fd = new FormData();
-    
     var fileInputElement = document.getElementById('videoFile');
     var files = fileInputElement.files; 
+    
+    if( ! files.length ) return; 
+    
+    $('#uploadStatusPanel').show(); 
+    var fd = new FormData();
     
     for(var i=0; i <= files.length; i++){
       fd.append("upload", files[i]);
@@ -79,16 +134,25 @@ $('#fileUpload').submit(function(e){
             if (this.status == 200) {
                 console.log("success");
                 clearInput('videoFile'); 
+                renderSelectedFilesTable(null); 
+                $('#uploadStatusPanel').hide(); 
+                $('#selectedFilesTable').hide(); 
             } else {
                 console.log("error " + this.status);
+                $('#uploadMsg').showMessage(this.statusText);
+                
             }
         }, 
         
         onprogress:  function(event) {
+            var percentLoaded = event.loaded * 100 / event.total; 
             console.log(event.loaded + ' / ' + event.total);
             console.log('percent ready: ', event.loaded * 100 / event.total); 
+            
+            updateProgressBar(parseInt(percentLoaded));  
         }
         
     }); 
     
 }); 
+
